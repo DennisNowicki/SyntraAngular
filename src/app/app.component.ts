b92948c29a14a09afe873d9e991c4b81deb1dc50
Dennis Nowicki <dennis.nowicki@utlook.com>
2020-01-26 15:12:34 +0100

    added ngOnInit and implemented interfece OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title : string;
-  name : string;
+export class AppComponent implements OnInit{
+  title: string;
+  name: string;
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {
     this.title = 'Nasa fake client database';
     this.name = 'Dennis';
   }
