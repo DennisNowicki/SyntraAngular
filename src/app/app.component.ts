4505565336d1dd7930e2192aed837ece052ad380
Dennis Nowicki <dennis.nowicki@utlook.com>
2020-01-26 15:19:29 +0100

    databinding with ng directives (*ngFor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title: string;
   name: string;
+
+  countries: string[];
 
   constructor() {}
 
   ngOnInit() {
     this.title = 'Nasa fake client database';
     this.name = 'Dennis';
+    this .countries = [ 'US' , 'Belgium' , 'Mozambique' , 'Antartica' ];
+
   }
 }
